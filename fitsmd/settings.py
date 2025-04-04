66cba5c990101ff668ebe817fd7688beecb731c3
Daniel Socianu <socianudaniel@gmail.com>
2025-04-04 23:41:09 +0200

    added allowed hosts

--- fitsmd/settings.py
+++ fitsmd/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY",default="unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','3.85.84.107','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
